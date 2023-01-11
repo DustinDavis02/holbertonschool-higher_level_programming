9bc79518ebf3d89b2bef938a4963170c37f306df
Dustin Davis <davisdustin328@gmail.com>
2023-01-10 23:49:59 -0600

    Adding formatting and files.

---
+++ 0x01-python-import_modules/1-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+argv = sys.argv[1:]
+n = len(argv)
+
+if n == 0:
+    print("{} arguments.\n".format(n))
+elif n == 1:
+    print("{} argument:\n".format(n))
+else:
+    print("{} arguments:\n".format(n))
+
+for i in range(n):
+    print("{}. {}\n".format(i + 1, argv[i]))
---
+++ 0x01-python-import_modules/2-variable_load.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+from variable_load_2 import a
+print("The value of variable a is: {}".format(a))
