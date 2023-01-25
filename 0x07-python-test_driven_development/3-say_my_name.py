935d23bb86aee968b4d677592d12a9ab286c80c1
Dustin Davis <davisdustin328@gmail.com>
2023-01-24 22:11:40 -0600

    First and last name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+Function that takes in 2 strings and prints first and last name
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Function that prints out First and last name
+    """
+    if type(first_name) != str:
+        raise TypeError("first_name must be a string")
+
+    if type(last_name) != str:
+        raise TypeError("last_name must be a string")
+
+    print("My name is {:s} {:s}".format(first_name, last_name))
