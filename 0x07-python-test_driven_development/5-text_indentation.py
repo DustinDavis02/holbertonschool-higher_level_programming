0b6eeced6d3e2ab1fbf5257b4fd4fd30a41c1753
Dustin Davis <davisdustin328@gmail.com>
2023-01-24 22:14:12 -0600

    Print newline off edgecase

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+Prints 2 newlines after '.,?,:'
+"""
+
+
+def text_indentation(text):
+    """
+    Prints input text with 2 new lines after our edgecases
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    for i in text:
+        print(i, end='')
+        if i in ".?:":
+            print("\n" * 2)
+    print("")
