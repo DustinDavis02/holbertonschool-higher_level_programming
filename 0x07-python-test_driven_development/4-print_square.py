4eaadf2670b06d3cab7ece5129426b9d5b1b7eaf
Dustin Davis <davisdustin328@gmail.com>
2023-01-24 22:12:06 -0600

    Prints adjustable square

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""
+prints square according to input parameter
+"""
+
+
+def print_square(size):
+    """
+    Takes the user input and prints a square out of #
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        print("#" * size)
