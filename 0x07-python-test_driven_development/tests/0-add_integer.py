ef6302cce5d32f78c64c6f65a68879c24004a3a2
Dustin Davis <davisdustin328@gmail.com>
2023-01-24 18:43:11 -0600

    Adding two integers

---
+++ 0x07-python-test_driven_development/tests/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Adding to integers (a + b)
+"""
+def add_integer(a, b=98):
+    """
+    Gives some of a + b
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    else:
+        return int(a) + int(b)
+    
