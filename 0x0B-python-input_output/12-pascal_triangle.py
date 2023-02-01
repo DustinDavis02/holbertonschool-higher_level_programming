e129f8dd1fed431941c875ba4de9b29175b03fdd
Dustin Davis <davisdustin328@gmail.com>
2023-02-01 11:43:20 -0600

    Pascal's triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""Pascal's triangle function"""
+
+
+def pascal_triangle(n):
+    """return list of integers rep Pascal's triangle"""
+    
+    if n<= 0:
+        return []
+    tri = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i+1):
+            if j == i:
+                row.append(1)
+            else:
+                row.append(tri[i-1][j-1] + tri[i-1][j])
+        tri.append(row)
+    return tri
