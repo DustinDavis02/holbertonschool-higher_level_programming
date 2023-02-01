cd5dacc56ff27720bab91c0795ef5b10aac560b1
Dustin Davis <davisdustin328@gmail.com>
2023-02-01 11:21:48 -0600

    load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
 """Load, add and save date to a file"""
+import sys
+import json
+
 
 
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-from sys import argv 
 
-args = argv[1:]
-try:
-    my_obj = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    objs = args
-else:
-    objs = my_obj + args
-finally:
-    save_to_json_file(objs, "add_item.json")
+
+def add_items():
+    items = []
+    for item in sys.argv[1:]:
+        items.append(item)
+        
+        with open("add_item.json", "w") as f:
+            json.dump(items, f)
+
+if __name__ == "__main__":
+    add_items()
