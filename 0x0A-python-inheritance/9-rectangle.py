1617aabccc5d71eda1fee1413c898a0ad9276a0e
Dustin Davis <davisdustin328@gmail.com>
2023-02-01 10:21:04 -0600

    Adds area to retangle

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-"""
-inherits from BaseGeometry with width and height and area implemented
-"""
+"""inherits from BaseGeometry with width and height and area implemented"""
 
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
-"""
- inherits from BaseGeometry
-"""
 
+
+class Retangle(BaseGeometry):
+        """inherits from BaseGeometry"""
 def __init__(self, width, height):
         self.integer_validator("width", width)
         self.integer_validator("height", height)
@@ -16,14 +14,10 @@ def __init__(self, width, height):
         self.__height = height
 
 def area(self):
-        """Method that calculates area of rectangle
-        """
-        
-        return self.__width * self.__height
+        """Method that calculates area of rectangle"""
+        area = self.__width * self.__height
+        return area
 
 def __str__(self):
-        """
-       return rectangle desc
-        """
-        
+        """return rectangle width and height"""
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
