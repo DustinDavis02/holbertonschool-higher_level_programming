5343069141ac3d3b621568433cd8c1853b25043c
Dustin Davis <davisdustin328@gmail.com>
2023-02-01 11:15:27 -0600

    style format

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,11 +3,7 @@
 Takes a readfile and displays it
 """
 
-
 def read_file(filename=""):
-    """
-    Takes in filename to read it
-    """
-
-    with open(filename, encoding="utf-8") as readFile:
-        print(readFile.read(), end='')
+    """Takes in filename to read and prints its content to the console."""
+    with open(filename, encoding="utf-8") as read_file:
+        print(read_file.read(), end='')
