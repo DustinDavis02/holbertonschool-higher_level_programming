154796b99aef1b68459e2c56237549c1331f9e26
Dustin Davis <davisdustin328@gmail.com>
2023-02-14 21:14:53 -0600

    Finds average

---
+++ 0x00-SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- Script that computes the score average of all records in second_table from the hbtn_0c_0 MYSQL database.
+-- Computes the score average of all records.
+SELECT AVG(score) AS average FROM second_table;
