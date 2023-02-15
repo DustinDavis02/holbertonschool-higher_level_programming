4cf5c557c9ee507509a7d37d13f4def12add934a
Dustin Davis <davisdustin328@gmail.com>
2023-02-14 21:11:13 -0600

    Update bob

---
+++ 0x00-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,5 @@
+-- Script that updates the score of bob to 10 in the second_table of the hbtn_0c_0 MYSQL database.
+-- Updates the score of bob.
+UPDATE second_table
+SET score = '10'
+WHERE name = 'Bob';
