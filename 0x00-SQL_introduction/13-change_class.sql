e70ba9bb96007b503823dd4c58fa16bd686949d1
Dustin Davis <davisdustin328@gmail.com>
2023-02-14 21:11:45 -0600

    Remove records

---
+++ 0x00-SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- Script that removes all records with a score of <= 5 from second_table in hbtn_0c_0 MYSQL database.
+-- Removes all records with a score <= 5.
+DELETE FROM second_table WHERE score <= 5;
