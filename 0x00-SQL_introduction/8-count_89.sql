c840ab6e43da2c8c4dd7a06aabd260e5099adecb
Dustin Davis <davisdustin328@gmail.com>
2023-02-14 21:09:52 -0600

    Display number of records with condition.

---
+++ 0x00-SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- Script displays the number of records with id 89 in the first_table of the hbtn_0c_0 MySQL database
+-- Displays number of records 
+SELECT COUNT(*) FROM first_table WHERE id = 89;
