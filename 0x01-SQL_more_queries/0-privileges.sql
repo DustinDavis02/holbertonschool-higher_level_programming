42ddb01fef3b2e51c3bce0904bfa449adf680c05
Dustin Davis <davisdustin328@gmail.com>
2023-02-14 23:09:46 -0600

    Lists all privileges

---
+++ 0x01-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- Script that List all privileges of users user_0d_1 and user_0d_2 on localhost server.
+-- List all privileges of users
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
