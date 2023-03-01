68e63bd590e11e3715929b13233c538b3417e5e6
Dustin Davis <davisdustin328@gmail.com>
2023-02-28 23:35:37 -0600

    Adding files

---
+++ 0x0B-python-object_relational_mapping/model_state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""contains the class definition of a State and an instance Base"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """State class that inherits from Base and links to the MySQL table states."""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
