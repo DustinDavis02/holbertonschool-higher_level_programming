0626ab89ba7ca5c02d5f92d6e804bc8bcd49e629
Dustin Davis <davisdustin328@gmail.com>
2023-03-01 21:13:16 -0600

    print state object

--- 0x0B-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0B-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,28 +1,21 @@
 #!/usr/bin/python3
 """Prints the State object with the name as argument"""
 
+import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from sys import argv
-from model_state import Base, State
+from model_state import State
+from sqlalchemy.orm import sessionmaker
 
-
-def get_state():
-    """Prints the State with the name passed as argument from the database hbtn_0e_6_usa"""
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(argv[1], argv[2], argv[3]),
-                           pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    session = Session(engine)
-
-    rows = session.query(State).filter(State.name == argv[4])
-    for state in rows:
-        print(state.id)
-    if rows is not argv[4]:
+if __name__ == '__main__':
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    toggle = False
+    for state in session.query(State):
+        if sys.argv[4] == state.name:
+            toggle = True
+            print("{}".format(state.id))
+    if not toggle:
         print("Not found")
-
-    session.close()
-
-
-if __name__ == "__main__":
-    get_state()
