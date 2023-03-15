75b79d1d5d4397faf6910683dd628dcf96a4def8
Dustin Davis <davisdustin328@gmail.com>
2023-03-14 22:07:27 -0500

    Send request display response

--- 0x0C-python-network_1/2-post_email.py
+++ 0x0C-python-network_1/2-post_email.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""A Python script that sends a request to a URL and displays the body of the response"""
+
+import urllib.request
+import urllib.error
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read()
+            print(body.decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
