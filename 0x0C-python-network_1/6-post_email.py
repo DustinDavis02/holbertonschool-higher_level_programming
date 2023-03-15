f57504e310410cf68511049e05e38b332f2c0cbf
Dustin Davis <davisdustin328@gmail.com>
2023-03-14 22:20:33 -0500

    Send post request with email

--- 0x0C-python-network_1/6-post_email.py
+++ 0x0C-python-network_1/6-post_email.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+Sends a POST request with an email to a given URL
+and displays the body of the response.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    payload = {'email': email}
+    response = requests.post(url, data=payload)
+    print(response.text)
