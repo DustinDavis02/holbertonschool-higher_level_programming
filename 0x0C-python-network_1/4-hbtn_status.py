f4b7a1e9684cf9d68884ba81f3433b84628d5955
Dustin Davis <davisdustin328@gmail.com>
2023-03-14 22:19:21 -0500

    Fetches status

--- 0x0C-python-network_1/4-hbtn_status.py
+++ 0x0C-python-network_1/4-hbtn_status.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""A Python script that fetches the intranet status page using the requests package"""
+
+import requests
+
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+    response = requests.get(url)
+
+    # Raise an exception for any status code that is not a success
+    response.raise_for_status()
+
+    # Display the body of the response in the specified format
+    data = response.json()
+    print("Body response:")
+    print("\t- type: {}".format(type(data)))
+    print("\t- content: {}".format(data))
