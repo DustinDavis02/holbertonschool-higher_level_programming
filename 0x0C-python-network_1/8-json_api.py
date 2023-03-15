ce30d07d28db542fa7ed8f160298f9a9d0e172ad
Dustin Davis <davisdustin328@gmail.com>
2023-03-14 22:21:12 -0500

    Send post request with letter as a parameter

--- 0x0C-python-network_1/8-json_api.py
+++ 0x0C-python-network_1/8-json_api.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""Script that takes a letter and sends a POST request to http://0.0.0.0:5000/search_user"""
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        q = ""
+    else:
+        q = sys.argv[1]
+
+    try:
+        url = 'http://0.0.0.0:5000/search_user'
+        data = {'q': q}
+        response = requests.post(url, data=data)
+
+        if response.json() == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.json().get('id'), response.json().get('name')))
+    except ValueError:
+        print("Not a valid JSON")
+    except:
+        print("Error: Could not connect to server")
