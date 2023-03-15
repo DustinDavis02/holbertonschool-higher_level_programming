add78a9ac87da573ceda9058376042275d3b840a
Dustin Davis <davisdustin328@gmail.com>
2023-03-14 22:20:20 -0500

    Send request and display header request id

--- 0x0C-python-network_1/5-hbtn_header.py
+++ 0x0C-python-network_1/5-hbtn_header.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""A Python script that sends a request to a URL and displays the value of the X-Request-Id header"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+
+    # Display the value of the X-Request-Id header if it exists
+    if "X-Request-Id" in response.headers:
+        print(response.headers["X-Request-Id"])
