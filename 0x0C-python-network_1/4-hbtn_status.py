08d48b2d5a923a4cc17e144f522876acc20ef9c0
Dustin Davis <davisdustin328@gmail.com>
2023-03-14 22:45:42 -0500

    Giving how to fetch status

--- 0x0C-python-network_1/4-hbtn_status.py
+++ 0x0C-python-network_1/4-hbtn_status.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-"""A Python script that fetches the intranet status page using the requests package"""
-
+"""
+Python script that fetches https://intranet.hbtn.io/status
+"""
 import requests
 
+
 if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"
+    url = 'https://intranet.hbtn.io/status'
     response = requests.get(url)
+    content = response.text
+    print('Body response:')
+    print('\t- type: {}'.format(type(content)))
+    print('\t- content: {}'.format(content))
 
-    # Raise an exception for any status code that is not a success
-    response.raise_for_status()
-
-    # Display the body of the response in the specified format
-    data = response.json()
-    print("Body response:")
-    print("\t- type: {}".format(type(data)))
-    print("\t- content: {}".format(data))
