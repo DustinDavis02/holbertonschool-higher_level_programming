12d443706b27e17a06d8e37b6b6b90048a923c95
Dustin Davis <davisdustin328@gmail.com>
2023-03-14 22:25:07 -0500

    Adding README

--- 0x0C-python-network_1/7-error_code.py
+++ 0x0C-python-network_1/7-error_code.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""
+Script that takes in a URL, sends a request to the URL and displays the
+body of the response.
+"""
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    response = requests.get(url)
+    status = response.status_code
+
+    if status >= 400:
+        print("Error code: {}".format(status))
+    else:
+        print(response.text)
