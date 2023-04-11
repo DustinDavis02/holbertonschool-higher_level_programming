15d89de193231ac4bc777e7598fda645f3cbfcba
Dustin Davis <davisdustin328@gmail.com>
2023-04-11 17:58:16 -0500

    Computes and prints fractorial

---
+++ 0x00-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+function factorial(n) {
+    if (Number.isNaN(n)) {
+      return 1;
+    } else if (n <= 1) {
+      return 1;
+    } else {
+      return n * factorial(n - 1);
+    }
+  }
+  const arg = process.argv[2];
+  const num = parseInt(arg);
+  console.log(factorial(num));
+  
