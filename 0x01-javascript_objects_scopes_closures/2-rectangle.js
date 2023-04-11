3f3531ff84f15c89f6b65ac89bf951e5b5362071
Dustin Davis <davisdustin328@gmail.com>
2023-04-11 18:24:18 -0500

    Same as before but if negative make empty

--- 0x01-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x01-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+class Rectangle {
+    constructor (w, h) {
+      if (w <= 0 || h <= 0) {
+        // Create an empty object if w or h is not a positive integer
+        return {};
+      }
+      // Initialize width and height instance attributes with values of w and h
+      this.width = w;
+      this.height = h;
+    }
+  }
+  module.exports = Rectangle;
+  
