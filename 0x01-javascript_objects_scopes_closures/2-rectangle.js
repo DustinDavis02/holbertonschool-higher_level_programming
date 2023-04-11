d00704a44e0e32d60f703d8c1210f1cc3239862a
Dustin Davis <davisdustin328@gmail.com>
2023-04-11 18:30:31 -0500

    Fixing rectangle

--- 0x01-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x01-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,14 +1,18 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor (w, h) {
+    constructor(w, h) {
       if (w <= 0 || h <= 0) {
-        // Create an empty object if w or h is not a positive integer
         return {};
       }
-      // Initialize width and height instance attributes with values of w and h
       this.width = w;
       this.height = h;
     }
-  }
-  module.exports = Rectangle;
   
+    print() {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+  
+  module.exports = Rectangle;
