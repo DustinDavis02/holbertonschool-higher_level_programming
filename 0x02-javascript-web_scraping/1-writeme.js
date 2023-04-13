ae5eca29843685cb9a58b8ee2bb64ad36cf28336
Dustin Davis <davisdustin328@gmail.com>
2023-04-13 14:55:05 -0500

    Changing write to method

--- 0x02-javascript-web_scraping/1-writeme.js
+++ 0x02-javascript-web_scraping/1-writeme.js
@@ -1,14 +1,5 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-
-const filePath = process.argv[2];
-const content = process.argv[3];
-
-fs.writeFile(filePath, content, 'utf-8', (err) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log(`Successfully wrote ${content.length} bytes to ${filePath}`);
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
 });
