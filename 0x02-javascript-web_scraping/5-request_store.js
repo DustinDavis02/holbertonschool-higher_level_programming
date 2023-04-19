c7d03befdec54dbbb6bdeaf3034eba708a3b8ba5
Dustin Davis <davisdustin328@gmail.com>
2023-04-19 09:54:28 -0500

    Task 5

--- 0x02-javascript-web_scraping/5-request_store.js
+++ 0x02-javascript-web_scraping/5-request_store.js
@@ -1,20 +1,5 @@
 #!/usr/bin/node
 
-const request = require('request');
 const fs = require('fs');
-
-const url = process.argv[2];
-const filePath = process.argv[3];
-
-request(url, function(error, response, body) {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log(body);
-    fs.writeFile(filePath, body, 'utf-8', function(error) {
-      if (error) {
-        console.error(error);
-      }
-    });
-  }
-});
+const request = require('request');
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
