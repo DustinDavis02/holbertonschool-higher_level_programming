e828a4253b8869f956802e8269982e09b54653a9
Dustin Davis <davisdustin328@gmail.com>
2023-04-19 09:42:42 -0500

    fixing output message

--- 0x02-javascript-web_scraping/5-request_store.js
+++ 0x02-javascript-web_scraping/5-request_store.js
@@ -6,16 +6,15 @@ const fs = require('fs');
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-request(url, (error, response, body) => {
+request(url, function(error, response, body) {
   if (error) {
     console.error(error);
-    return;
+  } else {
+    console.log(body);
+    fs.writeFile(filePath, body, 'utf-8', function(error) {
+      if (error) {
+        console.error(error);
+      }
+    });
   }
-  fs.writeFile(filePath, body, 'utf-8', (err) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log();
-  });
 });
