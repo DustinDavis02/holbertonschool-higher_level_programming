46678c2a2bfed1a663e056a250eb645af2d2d15c
Dustin Davis <davisdustin328@gmail.com>
2023-05-30 14:06:48 -0500

    Fixing

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
-"""Load, add and save date to a file"""
-from sys import argv
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+import sys
+from typing import List
+from os import path
+from json import dump
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+def add_arguments_to_list(args: List[str]) -> List[str]:
+    filename = "add_item.json"
+    my_list = []
 
-def add_items():
-    items = []
-    for item in sys.argv[1:]:
-        items.append(item)
-        
-        with open("add_item.json", "w") as f:
-            json.dump(items, f)
+    if path.exists(filename):
+        my_list = load_from_json_file(filename)
+
+    my_list.extend(args)
+    save_to_json_file(my_list, filename)
+
+    return my_list
 
 if __name__ == "__main__":
-    add_items()
+    arguments = sys.argv[1:]
+    add_arguments_to_list(arguments)
