e35275f6d5d68a2336c518f682575b74689c41f4
Dustin Davis <davisdustin328@gmail.com>
2023-05-30 14:07:43 -0500

    PEP8

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,6 +2,7 @@
 """Takes a readfile and displays it"""
 
 def read_file(filename=""):
-    """Takes in filename to read and prints its content to the console."""
-    with open(filename, encoding="utf-8") as read_file:
-        print(read_file.read(), end='')
+        """Takes in filename to read and prints its content to the console."""
+
+        with open(filename, encoding="utf-8") as read_file:
+            print(read_file.read(), end='')
